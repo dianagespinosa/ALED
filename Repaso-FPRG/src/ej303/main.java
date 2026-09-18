f8f4e3d17eeb30603ea6195e927cb6d036e961c3
dianagespinosa <diana.gespinosa@alumnos.upm.es>
2026-09-18 11:28:32 +0200

    Repaso en clase

--- Repaso-FPRG/src/ej303/main.java
+++ Repaso-FPRG/src/ej303/main.java
@@ -1,5 +1,16 @@
 package ej303;
 
 public class main {
+	
+	public static void main(String[] args) {
+		
+		int[] arr = new int[300];
+		for(int i = 0; i < 300; i++) {
+			for(int j = 0; j <= 16;i++) {
+				arr[i] = j;
+				System.out.print(arr[i]);
+			}
+		}
+	}
 
 }
